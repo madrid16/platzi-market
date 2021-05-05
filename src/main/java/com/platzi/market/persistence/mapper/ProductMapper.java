5b75ba813380c8fe21a68928fb71134e04ba4264
Madrid16 <m.madrid16@gmail.com>
2021-05-05 13:50:21 -0400

    implements repository with mapper, turn producto to product and product to producto

--- src/main/java/com/platzi/market/persistence/mapper/ProductMapper.java
+++ src/main/java/com/platzi/market/persistence/mapper/ProductMapper.java
@@ -21,7 +21,7 @@ public interface ProductMapper {
             @Mapping(source = "categoria", target = "category")
     })
     Product toProduct(Producto producto);
-    List<Product> toProducts(List<Product> productos);
+    List<Product> toProducts(List<Producto> productos);
 
     @InheritInverseConfiguration
     @Mapping(target = "codigoBarras", ignore = true)
