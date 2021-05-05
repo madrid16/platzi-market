06535634ee5cbe894a6a3d828037f0d520091542
Madrid <madrid@MacBook-Pro-de-Edgar.local>
2021-05-05 01:23:48 -0400

    add entities to project with pgsql database from docker instance

---
+++ src/main/java/com/platzi/market/persistence/entity/Compra.java
@@ -0,0 +1,31 @@
+package com.platzi.market.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "compras")
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    private String comentario;
+
+    private String estado;
+}
